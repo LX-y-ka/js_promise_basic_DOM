2a66e3cde05e3ffc607b45900ebcc15560e5c4ea
LX-y-ka <alexandravirtualife@gmail.com>
2025-01-08 13:00:12 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject, 3000);
+});
+
+promise1.then(
+  () => {
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.innerText = 'Promise was resolved!';
+  },
+  () => {
+    const div = document.createElement('div');
+
+    div.classList.add('message', 'error-message');
+    div.innerText = 'Promise was rejected!';
+  },
+);
+
+promise2.then(
+  () => {
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.innerText = 'Promise was resolved!';
+  },
+  () => {
+    const div = document.createElement('div');
+
+    div.classList.add('message', 'error-message');
+    div.innerText = 'Promise was rejected!';
+  },
+);
